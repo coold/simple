d9b6f596405987729d2e650f9946e7903b93b501
coold <rdcoold@gmail.com>
2013-05-29 01:26:22 +0600

    Commit after adding spork and guard

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+let(:rep){"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
@@ -12,7 +12,7 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => rep+" | Home")
     end
   end
 
@@ -26,21 +26,33 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => rep+" | Help")
     end
+  end
+
+  describe "Contact page" do
+  	it "should have the h1 'Contact'" do
+  	visit '/static_pages/contact'
+      page.should have_selector('title', :text => 'Contact')
+ 	end
   end
 
   describe "About page" do
 
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => rep+" | About Us")
+    end
+
+    it "should have the div 'ololo'" do
+      visit '/static_pages/about'
+      page.should have_selector('div', text: "ololo")
     end
   end
 end
